5706af6000e6294dd975a02328c1f87eb96ad5ea
Konstantin Skibin <Konst.SKI@yandex.ru>
2026-06-03 12:40:19 +0700

    Удалены старые файлы

--- Interpreter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vector>
-#include "LL1Parser.h" // Подключаем, чтобы компилятор знал структуру RpnElement
-
-class Interpreter {
-public:
-    // Главная функция выполнения ОПС
-    static void execute(const std::vector<RpnElement>& rpn);
-};
