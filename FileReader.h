7ad5e093f9da27fed32cf9071ec6b3eb6032c856
Konstantin Skibin <Konst.SKI@yandex.ru>
2026-06-04 20:40:08 +0700

    Удалены лишние файлы

--- FileReader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-// Статический класс для чтения исходных файлов
-class FileReader {
-public:
-    // Считывает файл и удаляет символы \t и \r
-    static std::string read(const std::string& filePath);
-};
